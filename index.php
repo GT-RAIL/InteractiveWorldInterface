94f09f207d2b8547ede3b40f0fed65624ffccc08
Russell Toris <rctoris@wpi.edu>
2013-03-08 12:19:40 -0800

    reworking of interface size

--- index.php
+++ index.php
@@ -1,13 +1,14 @@
 <?php
 /**
- * A basic interface to display 1 or more MJPEG streams and basic keyboard teleoperation control.
+ * An interface to provide remote demonstrations for use with certain LfD algorithms and the PR2
+ * robot.
  *
  * @author     Russell Toris <rctoris@wpi.edu>
- * @copyright  2013 Russell Toris, Worcester Polytechnic Institute
+ * @copyright  2013 Robert Bosch LLC
  * @license    BSD -- see LICENSE file
- * @version    December, 13 2012
- * @package    api.robot_environments.interfaces.basic
- * @link       http://ros.org/wiki/rms
+ * @version    March, 8 2013
+ * @package    api.robot_environments.interfaces.bosch_pr2_remote_demonstration_interface
+ * @link       http://ros.org/wiki/bosch_pr2_remote_demonstration_interface
  */
 
 /**
@@ -15,9 +16,9 @@
  * @param robot_environment $re The associated robot_environment object for this interface
  */
 function generate($re) {
-	// lets begin by checking if we have an MJPEG widget and a keyboard at the very least
-	if(!$re->get_widgets_by_name('MJPEG Stream')) {
-		create_error_page('No MJPEG streams found.', $re->get_user_account());
+	// check if we have all the widget information we have
+	if(count($re->get_widgets_by_name('MJPEG Stream')) < 2) {
+		create_error_page('Not enough MJPEG streams.', $re->get_user_account());
 	} else if(!$teleop = $re->get_widgets_by_name('Keyboard Teleop')) {
 		create_error_page('No Keyboard Teloperation settings found.', $re->get_user_account());
 	} else if(!$re->authorized()) {
@@ -28,16 +29,21 @@ function generate($re) {
 <head>
 	<?php $re->create_head() // grab the header information ?>
 <script type="text/javascript"
-	src="https://raw.github.com/RobotWebTools/rosbagjs/groovy-devel/topiclogger.js"></script>
+  src="https://raw.github.com/RobotWebTools/pr2runstopjs/groovy-devel/pr2runstop.js"></script>
+<script type="text/javascript"
+  src="https://raw.github.com/RobotWebTools/rosbagjs/groovy-devel/topiclogger.js"></script>
 <title>Basic Teleop Interface</title>
 
 	<?php $re->make_ros() // connect to ROS ?>
 
 <script type="text/javascript">
   ros.on('error', function() {
-  	alert('Lost communication with ROS.');
+    alert('Lost communication with ROS.');
   });
 
+  /**
+   * Setup an JavaScrip widgets we need once the page is loaded.
+   */
   function start() {
     // initialize the run-stop widget
     var pr2RunStop = new PR2RunStop({
@@ -54,46 +60,54 @@ function generate($re) {
 
     // create the global display
     var rmsDisplay = new RMSDisplay({
-    	ros : ros,
-    	divID : 'scene',
-    	width : 800,
-    	height : 600,
-    	background : '#022056',
-    	gridColor : '#9CCDFC'
+      ros : ros,
+      divID : 'scene',
+      width : 1180,
+      height : 600,
+      background : '#101010',
+      gridColor : '#BD4647'
+    });
+
+    // setup the buttons
+    $('body').bind('DOMSubtreeModified', function() {
+      $('button').button();
     });
   }
 </script>
 </head>
 <body onload="start()">
-	<section id="interface">
-
-
-		<table class="center">
-			<tr>
-				<td width="33%"><div id="run-stop"></div></td>
-				<td><h1>PR2 Remote Demonstrations</h1></td>
-			</tr>
-			<tr>
-				<td width="33%"><div id="control-widget">
-						<div id="speed-container">
-						<?php echo create_keyboard_teleop_with_slider($teleop[0])?>
-						</div>
-					</div></td>
-				<td><div id="scene"></div></td>
-			</tr>
-			<tr>
-				<td width="33%"><div id="south-west-widget">
-						<?php echo create_multi_mjpeg_canvas_by_envid($re->get_envid(), 266, 200, 1)?>
-					</div>
-				</td>
-				<td width="66%"></td>
-			</tr>
-		</table>
-		<div id="logger"></div>
-	</section>
+  <section id="interface">
+    <table>
+      <tr>
+        <td rowspan="2">
+          <div id="run-stop"></div>
+          <div id="speed-container">
+          <?php echo create_keyboard_teleop_with_slider($teleop[0])?>
+          </div>
+          <div id="video1">
+          <?php echo create_multi_mjpeg_canvas_by_envid($re->get_envid(), 400, 300, 0)?>
+          </div>
+          <div id="video2">
+          <?php echo create_multi_mjpeg_canvas_by_envid($re->get_envid(), 400, 300, 1)?>
+          </div>
+        </td>
+        <td><h2>PR2 Remote Demonstrations</h2></td>
+        <td><img src="../img/logo.png"></td>
+      </tr>
+      <tr>
+        <td colspan="2"><div id="scene"></div></td>
+      </tr>
+      <tr>
+        <td colspan="3">
+          <div id="logger"></div>
+        </td>
+      </tr>
+    </table>
+    <?php create_footer()?>
+  </section>
 </body>
 </html>
-<?php
-  }
+          <?php
+	}
 }
 ?>
