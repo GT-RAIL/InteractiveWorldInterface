d6cb7be3a70f5c9e3eb733f1404e4a0ebeff7ac4
Russell Toris <rctoris@wpi.edu>
2014-12-08 13:37:06 -0500

    changed controller name

--- app/Controller/IwmodelsController.php
+++ app/Controller/IwmodelsController.php
@@ -11,7 +11,7 @@
  * @version		0.2.0
  * @package		app.Controller
  */
-class IwmodelController extends AppController {
+class IwmodelsController extends AppController {
 
 /**
  * The used components for the controller.
